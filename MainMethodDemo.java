8671cf5e5f196f9be3b819f17e0387844aeca935
Vinodkumar799365 <mudavathvinodkumar799@gmail.com>
2025-10-24 13:36:43 +0530

    Add files via upload

---
+++ MainMethodDemo.java
@@ -0,0 +1,7 @@
+public class MainMethodDemo{
+       public static void main(String[] args){
+              System.out.println("my name:"+args[0]);
+              System.out.println("my fathers name:"+args[1]);
+              System.out.println("my brothers name:"+args[2]);
+       }
+}
